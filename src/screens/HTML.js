1cfbe7cb29ecb889e85eef40b0098b8d73f12c54
Adarsh077 <adarshco077@gmail.com>
2020-05-18 20:32:23 +0530

    file upload bug fix

--- src/screens/HTML.js
+++ src/screens/HTML.js
@@ -12,6 +12,7 @@ export class HTML extends Component {
       showSettings: false,
     };
     this.htmlEditorRef = createRef();
+    this.fileInputRef = createRef();
   }
 
   componentDidMount() {
